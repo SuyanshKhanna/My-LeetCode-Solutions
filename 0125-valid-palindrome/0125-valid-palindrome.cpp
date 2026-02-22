b9ca1d825881476660ed721dcb9abb0edf5f2b65
SuyanshKhanna <suyansh.khanna07@gmail.com>
2026-02-22 13:41:49 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-
-    bool isNumAlpha(char ch)
-    {
-        ch = tolower(ch);
-        return (ch >= 97 && ch <=122 ||
-                ch >= 48 && ch <= 57);
-    }
+    // bool isNumAlpha(char ch) {
+    //     ch = tolower(ch);
+    //     return (ch >= 97 && ch <= 122 || ch >= 48 && ch <= 57);
+    // }
     bool isPalindrome(string s) {
-        int i=0,j=s.size() - 1;
-        while(i<=j)
-        {
-            if(isNumAlpha(s[i]) && isNumAlpha(s[j]) && tolower(s[i]) != tolower(s[j]))
-            return false;
-
-            else if(!isNumAlpha(s[i]))
-            i++;
-
-            else if(!isNumAlpha(s[j]))
-            j--;
-
-            else
-            {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isalnum(s[i]))
+                i++;
+            else if (!isalnum(s[j]))
+                j--;
+            else if (tolower(s[i]) != tolower(s[j]))
+                return false;
+            else {
                 i++;
                 j--;
             }
+        }
+        return true;
 
-            
-        }return true;
+        // while(i<=j)
+        // {
+        //     if(isNumAlpha(s[i]) && isNumAlpha(s[j]) && tolower(s[i]) !=
+        //     tolower(s[j])) return false;
+
+        //     else if(!isNumAlpha(s[i]))
+        //     i++;
+
+        //     else if(!isNumAlpha(s[j]))
+        //     j--;
+
+        //     else
+        //     {
+        //         i++;
+        //         j--;
+        //     }
+
+        // }return true;
     }
 };
