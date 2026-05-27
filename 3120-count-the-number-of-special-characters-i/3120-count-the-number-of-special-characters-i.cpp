729c4f25afbbcfc3cb38091c61775d6bb44d896b
SUYANSH KHANNA <suyansh.khanna07@gmail.com>
2026-05-27 23:42:44 +0530

    Time: 0 ms (100%), Space: 9.7 MB (19.68%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        /* sort(nums.begin(), nums.end());
-        int len = nums.size();
-        unordered_map<int, int> freq;
-        int maximum =0;
-        for( auto x : nums){
-            maximum = max(maximum, x);
-            freq[x]++;
-        }
-        int freqS=freq.size();
-        for(auto x : freq){
-            if(x.first != maximum && x.second !=1)
-                return false;
-        }
-        if(freq[maximum]==2 && len == maximum+1)
-            return true;
-        return false;*/
         int len = word.size();
         int special=0;
         unordered_map<char,int> count;
