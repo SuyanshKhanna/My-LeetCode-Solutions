500af9b4e051e1d83a9eef3fa6719c82c8b6e684
SUYANSH KHANNA <suyansh.khanna07@gmail.com>
2026-06-10 23:54:27 +0530

    Time: 3 ms (79.82%), Space: 14.6 MB (87.74%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
+    // int firstUniqChar(string s) {
+    //     int len = s.size();
+    //     unordered_map<char, pair<int, int>> freq;
+    //     for(int i=0; i<len; i++){
+    //         freq[s[i]].first++;
+    //         freq[s[i]].second = i;
+    //     }
+    //     int mini = INT_MAX;
+    //     for(auto x: freq){
+    //         if(x.second.first>1)
+    //             continue;
+    //         mini = min(mini, x.second.second);
+    //     }
+    //     if(mini == INT_MAX)
+    //         return -1;
+    //     return mini;
+    // }
+
     int firstUniqChar(string s) {
-        int len = s.size();
-        unordered_map<char, pair<int, int>> freq;
-        for(int i=0; i<len; i++){
-            freq[s[i]].first++;
-            freq[s[i]].second = i;
+
+        int count[26] = {0};
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            count[s[i] - 'a']++;
         }
-        int mini = INT_MAX;
-        for(auto x: freq){
-            if(x.second.first>1)
-                continue;
-            mini = min(mini, x.second.second);
+
+        for (int i = 0; i < len; i++) {
+            if (count[s[i] - 'a'] == 1) {
+                return i;
+            }
         }
-        if(mini == INT_MAX)
-            return -1;
-        return mini;
+
+        return -1;
     }
 };
