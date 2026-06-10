3a700c16cb3a3f43af3d5f789130664f2f35f99a
SUYANSH KHANNA <suyansh.khanna07@gmail.com>
2026-06-10 23:48:55 +0530

    Time: 18 ms (26.62%), Space: 14.8 MB (54.18%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int len = s.size();
+        unordered_map<char, pair<int, int>> freq;
+        for(int i=0; i<len; i++){
+            freq[s[i]].first++;
+            freq[s[i]].second = i;
+        }
+        int mini = INT_MAX;
+        for(auto x: freq){
+            if(x.second.first>1)
+                continue;
+            mini = min(mini, x.second.second);
+        }
+        if(mini == INT_MAX)
+            return -1;
+        return mini;
+    }
+};
