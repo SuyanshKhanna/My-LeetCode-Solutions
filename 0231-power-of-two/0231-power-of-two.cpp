8709345c4b71160e03863bc52ba02a17a1bd772d
SUYANSH KHANNA <suyansh.khanna07@gmail.com>
2026-09-13 23:56:02 +0530

    Time: 0 ms (100%), Space: 8 MB (12.78%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<1)
             return false;
-       if( (abs(n) & (abs(n)-1))==0)
+       if( (n & (n-1))==0)
             return true;
         return false;
     }
