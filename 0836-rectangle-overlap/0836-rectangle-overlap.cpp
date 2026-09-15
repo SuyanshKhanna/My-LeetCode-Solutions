fe91b04ec051e0385e5ae70bd482bf6aa01b1b24
SUYANSH KHANNA <suyansh.khanna07@gmail.com>
2026-09-15 23:56:48 +0530

    Time: 0 ms (100%), Space: 9.7 MB (77.15%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        if((rec2[0]<rec1[2] && rec2[1]<rec1[3]) && (rec1[0]<rec2[2] && rec1[1]<rec2[3]) ) return true;
-        //else if(rec1[0]<rec2[2] && rec1[1]<rec2[3]) return true;
+        if(rec2[0]<rec1[2] && rec2[1]<rec1[3]&& (rec1[0]<rec2[2] && rec1[1]<rec2[3])){
+            cout<<"true from here";
+            return true;
+        }
+        // else if(rec1[0]<rec2[0] && rec1[1]<rec2[1])
+        //     return true;
         return false;
     }
-
 };
